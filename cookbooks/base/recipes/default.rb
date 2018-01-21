57e7226491fa45ac439f34b88760fbf2f2f6fd43
Matt Feng <matt.feng@yahoo.com>
2018-01-21 16:01:56 +0000

    added more to base cookbook

--- cookbooks/base/recipes/default.rb
+++ cookbooks/base/recipes/default.rb
@@ -17,3 +17,20 @@ service 'httpd' do
   action [:enable, :start]
 end
 
+#includes websites.rb
+#include_recipe 'apache::websites'
+
+#excuting a command
+execute 'just a test' do
+  command "echo hi > /tmp/test.txt"
+  action :run
+end
+
+#adding a user
+user 'mfeng' do
+  uid 2000
+  shell '/bin/zsh'
+  action :create
+end 
+
+
